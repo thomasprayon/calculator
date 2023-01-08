01f1461be03fbd72cc03fcc14657219143a41327
thomasprayon <thomasprayongowland@gmail.com>
2023-01-08 21:14:20 +0100

    Operator function

--- script.js
+++ script.js
@@ -19,3 +19,22 @@ function multiply(value1, value2) {
 function divide(value1, value2) {
     return value1 / value2;
 }
+
+// OPERATE
+
+function operate(operator, val1, val2) {
+    switch (operator) {
+        case '+':
+            return add(val1, val2);
+        case '-':
+            return subtract(val1, val2);
+        case '*':
+            return multiply(val1, val2);
+        case '/':
+            return divide(val1, val2);
+        default:
+            return null;
+    }
+}
+
+operate('+', 5, 5);
