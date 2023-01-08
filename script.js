790a7f0a5f15692d4c773ba9c14784efac54e2b4
thomasprayon <thomasprayongowland@gmail.com>
2023-01-08 20:58:17 +0100

    Basic math operators functions

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log('Script attached to HTML file ;)');
+
+// ADD
+function add(value1, value2) {
+    return value1 + value2;
+}
+
+// SUBTRACT
+function subtract(value1, value2) {
+    return value1 - value2;
+}
+
+// MULTIPLY
+function multiply(value1, value2) {
+    return value1 * value2;
+}
+
+// DIVIDE
+function divide(value1, value2) {
+    return value1 / value2;
+}
